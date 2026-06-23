f5169da7e494440ef8fc62d319f8ce8264281e6d
Ayush Yadav <ayushyadav1355@gmail.com>
2026-06-23 20:51:11 +0530

    commite changes for deploy

--- CSE19React/vite.config.js
+++ CSE19React/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  base: '/React_project/', // <-- Add this exact line here
 })
