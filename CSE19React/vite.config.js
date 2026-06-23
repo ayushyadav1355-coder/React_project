3433ac6d713eba8b5e5a3b25fdb9a80da3d72a84
Ayush Yadav <ayushyadav1355@gmail.com>
2026-06-23 22:02:20 +0530

    commit changes

--- CSE19React/vite.config.js
+++ CSE19React/vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  base: '/React_project/', // <-- Add this exact line here
+  plugins: [react()],
+  base: '/React_project/', 
 })
-  import react, { reactCompilerPreset } from '@vitejs/plugin-react'
